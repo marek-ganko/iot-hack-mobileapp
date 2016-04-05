be100a4d7f694c6b685ff014e433aa7ceeabb350
mateuszsikora <mateusz.sikora@nokia.com>
2016-04-06 00:31:52 +0200

    return static position when location not available

--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -1,22 +1,27 @@
 import {Page} from 'ionic-angular';
+import {ElementRef} from 'angular2/core';
 import {Geolocation} from "ionic-native/dist/index";
 
 @Page({
   templateUrl: 'build/pages/page1/page1.html',
 })
 export class Page1 {
+  private elementRef:ElementRef;
 
-  constructor() {
+  constructor(elementRef:ElementRef) {
+    this.elementRef = elementRef;
+
     this.loadMap().then(map => {
-
       this.loadBicycleLayer(map);
       this.loadHeatmapLayer(map);
-
     });
+
   }
+
 
   private loadHeatmapLayer(map) {
     this.getPoints().then(points => {
+      console.log('a')
       new google.maps.visualization.HeatmapLayer({
         data: points,
         map: map,
@@ -33,12 +38,27 @@ export class Page1 {
   }
 
   private getCurrentPosition():Promise<any> {
-    return Geolocation.getCurrentPosition({timeout: 10000, enableHighAccuracy: true});
+    return Geolocation.getCurrentPosition({timeout: 10000, enableHighAccuracy: true}).then(position=>position, ()=> {
+      console.error('getCurrentPosition error');
+      return {
+        timestamp: new Date().getTime(),
+        coords: {
+          latitude: 51.126603100000004,
+          longitude: 16.9779427,
+          altitude: 0,
+          accuracy: 63,
+          altitudeAccuracy: 0,
+          heading: NaN,
+          speed: NaN
+        }
+      };
+    });
   }
 
   private loadMap():Promise<any> {
     return new Promise((resolve => {
       this.getCurrentPosition().then(position => {
+        console.log(position)
         const latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
         const mapOptions = {
           center: latLng,
@@ -60,13 +80,13 @@ export class Page1 {
         new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
         new google.maps.LatLng(position.coords.latitude, position.coords.longitude),
-        new google.maps.LatLng(position.coords.latitude+0.01, position.coords.longitude+0.01),
-        new google.maps.LatLng(position.coords.latitude+0.011, position.coords.longitude+0.01),
-        new google.maps.LatLng(position.coords.latitude+0.012, position.coords.longitude+0.01),
-        new google.maps.LatLng(position.coords.latitude+0.013, position.coords.longitude+0.01),
-        new google.maps.LatLng(position.coords.latitude+0.02, position.coords.longitude+0.02),
-        new google.maps.LatLng(position.coords.latitude+0.03, position.coords.longitude+0.03),
-        new google.maps.LatLng(position.coords.latitude+0.04, position.coords.longitude+0.04),
+        new google.maps.LatLng(position.coords.latitude + 0.01, position.coords.longitude + 0.01),
+        new google.maps.LatLng(position.coords.latitude + 0.011, position.coords.longitude + 0.01),
+        new google.maps.LatLng(position.coords.latitude + 0.012, position.coords.longitude + 0.01),
+        new google.maps.LatLng(position.coords.latitude + 0.013, position.coords.longitude + 0.01),
+        new google.maps.LatLng(position.coords.latitude + 0.02, position.coords.longitude + 0.02),
+        new google.maps.LatLng(position.coords.latitude + 0.03, position.coords.longitude + 0.03),
+        new google.maps.LatLng(position.coords.latitude + 0.04, position.coords.longitude + 0.04),
       ];
     });
   }
