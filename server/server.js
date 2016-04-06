3c5db7226dda000c1d37b985517157fff969710e
mateuszsikora <mateusz.sikora@nokia.com>
2016-04-06 05:01:57 +0200

    add geohash

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 Measurments = new Meteor.Collection("measurments");
+var Geohash = Meteor.npmRequire('latlon-geohash');
 
 if(Meteor.isServer) {
   Meteor.startup(function () {
@@ -8,7 +9,38 @@ if(Meteor.isServer) {
       prettyJson: true
     });
 
-    Api.addCollection(Measurments, 'measurments');
-  
+    Api.addRoute('measurments', {
+
+      get: {
+        action: function () {
+          var measurments = Measurments.find({}).fetch();
+          if (measurments) {
+            return {status: "success", data: measurments};
+          }
+          return {
+            statusCode: 400,
+            body: {status: "fail", message: "Unable to add article"}
+          };
+        }
+      },
+
+      post: {
+        action: function () {
+          var body = this.request.body;
+          var position = body.position;
+          body.geohash = Geohash.encode(position.lat, position.lon);
+          var article = Measurments.insert(body);
+
+          if (article) {
+            return {status: "success", data: article};
+          }
+
+          return {
+            statusCode: 400,
+            body: {status: "fail", message: "Unable to add article"}
+          };
+        }
+      }
+    });
   });
 }
