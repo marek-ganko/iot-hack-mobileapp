ae5763a3a2472db6560b15113b01f85e6df057af
MaciejBaj <macie.baj@gmail.com>
2016-04-06 08:06:44 +0200

    if deploy works check

--- server/deploy.sh
+++ server/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-meteor build bundle.tar.gz
-scp bundle.tar.gz iot-hackaton@185.5.97.71:/home/iot-hackaton/bundle.tar.gz
-ssh iot-hackaton@185.5.97.71 'tar xf bundle.tar.gz && cd bundle/programs/server && npm install && export MONGO_URL='mongodb://healthy-way:healthy-way@localhost:27018/healthy-way' && export ROOT_URL='http://localhost' && export PORT=3000 && forever stopall && forever start ../../main.js'
-rm -r bundle.tar.gz
+meteor build bundle
+ssh iot-hackaton@185.5.97.71 'forever stopall && rm -r bundle bundle.tar.gz'
+scp bundle/server.tar.gz iot-hackaton@185.5.97.71:/home/iot-hackaton/bundle.tar.gz
+ssh iot-hackaton@185.5.97.71 "tar xf bundle.tar.gz && cd bundle/programs/server && npm install && export MONGO_URL='mongodb://healthy-way:healthy-way@localhost:27018/healthy-way' && export ROOT_URL='http://localhost' && export PORT=3000 && forever start ../../main.js"
+rm -r bundle
