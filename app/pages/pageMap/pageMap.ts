0878a07b5b58d06016dd58e6f69733252eec2e43
MaciejBaj <macie.baj@gmail.com>
2016-04-06 07:42:02 +0200

    gradient for humanidity

--- app/pages/pageMap/pageMap.ts
+++ app/pages/pageMap/pageMap.ts
@@ -24,6 +24,7 @@ export class PageMap {
         data = data.data ? this.formatDataCoords(data.data) : null;
         console.log(data);
         this.heatmapLayer.setData(data ? data : null);
+        this.heatmapLayer.set('gradient', this.heatmapLayer.get('gradient') ? null :  PageMap.HUMIDITY_GRADIENT);
       },
       err => console.error(err)
     );
@@ -194,6 +195,25 @@ export class PageMap {
 
   ngOnDestroy() {
     this.watchPositionSubscription.unsubscribe();
+  }
+
+  private static get HUMIDITY_GRADIENT() {
+    return [
+      'rgba(0, 255, 255, 0)',
+      'rgba(0, 255, 255, 1)',
+      'rgba(0, 191, 255, 1)',
+      'rgba(0, 127, 255, 1)',
+      'rgba(0, 63, 255, 1)',
+      'rgba(0, 0, 255, 1)',
+      'rgba(0, 0, 223, 1)',
+      'rgba(0, 0, 191, 1)',
+      'rgba(0, 0, 159, 1)',
+      'rgba(0, 0, 127, 1)',
+      'rgba(63, 0, 91, 1)',
+      'rgba(127, 0, 63, 1)',
+      'rgba(191, 0, 31, 1)',
+      'rgba(255, 0, 0, 1)'
+    ];
   }
 }
   
